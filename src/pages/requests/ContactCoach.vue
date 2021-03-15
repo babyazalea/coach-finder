72358b716a4c9160d452737c7ae96d094f0e6ec8
babyazalea <usun12@hotmail.com>
2021-03-15 14:08:44 +0900

    message-state store에 추가

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -34,9 +34,16 @@ export default {
         !this.email.includes('@') ||
         this.message === ''
       ) {
-        this.formIsValid === false;
+        this.formIsValid = false;
         return;
       }
+
+      this.$store.dispatch('requests/contactCoach', {
+        email: this.email,
+        message: this.message,
+        coachId: this.$route.params.id
+      });
+      this.$router.replace('/coaches');
     }
   }
 };
