43dc726e67dd43c79ea2633ef65c8faf65cdd438
babyazalea <usun12@hotmail.com>
2021-03-16 14:16:52 +0900

    adding singup-logic

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import { createApp } from 'vue';
 import router from './router.js';
 import store from './store/index';
 import App from './App.vue';
+import dotenv from 'dotenv';
 
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
 import BaseSpinner from './components/ui/BaseSpinner.vue';
 import BaseDialog from './components/ui/BaseDialog.vue';
+
+dotenv.config();
 
 const app = createApp(App);
 
